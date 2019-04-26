4f64170a069a69211cc1f0f8d7b5ae7da4dca98b
Peter Kropf <peterk@ashbylumber.com>
2019-04-26 15:26:05 -0700

    added description and example usage comments to upper_lower_check.

--- functions.py
+++ functions.py
@@ -1,6 +1,30 @@
 import json
 
 def upper_lower_check(topic, message, section, srv):
+    """perform a range check on a value in the json formatted
+    message. controls live in the configuration file. for example,
+    a control section set up as:
+
+        [temperature/somewhere]
+        topic = sensor/somewhere
+        targets = slack:environment
+        format = Somewhere trouble with temperature {temperature}C
+        check = temperature
+        lower = 18
+        upper = 24
+        filter = upper_lower_check()
+
+    when a message is received as:
+
+        {"temperature": "19.0", "humidity": "73.0"}
+
+    will not send a message to slack but a message as:
+
+        {"temperature": "25.0", "humidity": "73.0"}
+
+    will send "Somewhere trouble with temperature 25.0" to the
+    configured slack channel.
+    """
     #srv.logging.debug(f"topic: {topic}")
     #srv.logging.debug(f"message: {message}")
     #srv.logging.debug(f"section: {section}")
